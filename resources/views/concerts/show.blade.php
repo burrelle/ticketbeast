6685834f09d45069b9e25c6110dd55f38c7c0256
Evan Burrell <burrelle@oregonstate.edu>
2018-11-30 14:31:14 +0000

    Refactoring for speed

--- resources/views/concerts/show.blade.php
+++ resources/views/concerts/show.blade.php
@@ -1,7 +1,7 @@
 <h1>{{ $concert->title }}</h1>
 <h2>{{ $concert->subtitle }}</h2>
 <p>{{ $concert->formatted_date }}</p>
-<p>Doors at {{ $concert->date->format('g:i a')}}</p>
+<p>Doors at {{ $concert->date->formatted_start_time }}</p>
 <p>{{ number_format($concert->ticket_price/100, 2) }}</p>
 <p>{{ $concert->venue }}</p>
 <p>{{ $concert->venue_address }}</p>
--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -7,19 +7,30 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ConcertTest extends TestCase
 {
-    use DatabaseMigrations;
-
     public function testCanGetFormattedDate()
     {
-        // Create a concert with a known date
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 8:00 pm')
         ]);
 
-        // Retrieve the formatted date
-        $date = $concert->formatted_date;
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
 
-        // Verify the date is formatted
-        $this->assertEquals('December 1, 2016', $date);
+    public function testCanGetFormattedStartTime()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00')
+        ]);
+
+        $this->assertEquals('5:00 pm', $concert->formatted_start_time);
+    }
+
+    public function testCanGetTicketPriceInDollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => '6750'
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
     }
 }
