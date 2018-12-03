391c54f3be2ad18fa770003e48f284d2b0d5ca8f
Evan Burrell <burrelle@oregonstate.edu>
2018-12-03 09:42:21 +0000

    Getting started with validation testing

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -10,22 +10,40 @@ class PurchaseTicketsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
     public function testCustomerCanPurchaseConcertTickets()
     {
-        $paymentGateway = new FakePaymentGateway;
-        $this->app->instance(PaymentGateway::class, $paymentGateway);
         $concert = factory(Concert::class)->create(['ticket_price' => '3250']);
         $response = $this->json('POST', "/concerts/{$concert->id}/orders", [
             'email' => 'john@example.com',
             'ticket_quantity' => 3,
-            'payment_token' => $paymentGateway->getValidTestToken()
+            'payment_token' => $this->paymentGateway->getValidTestToken()
         ]);
 
         $response->assertStatus(201);
-        $this->assertEquals(9750, $paymentGateway->totalCharges());
+        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
 
         $order = $concert->orders()->where('email', 'john@example.com')->first();
         $this->assertNotNull($order);
         $this->assertEquals(3, $order->tickets->count());
+    }
+
+    public function testEmailIsRequiredToPurchaseTickets()
+    {
+
+        $concert = factory(Concert::class)->create();
+        $response = $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'ticket_quantity' => 3,
+            'payment_token' => $this->paymentGateway->getValidTestToken()
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertArrayHasKey('errors', $response->decodeResponseJson());
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function disableExceptionHandling(){
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(Exception $e) {}
+            public function render($request, Exception $e){
+                throw $e;
+            }
+        });
+    }
 }
