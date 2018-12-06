2462c126d1fc2ed34b870741320d37caffd23df3
Evan Burrell <burrelle@oregonstate.edu>
2018-12-06 13:34:39 +0000

    Returning order details

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -37,9 +37,13 @@ class PurchaseTicketsTest extends TestCase
             'payment_token' => $this->paymentGateway->getValidTestToken()
         ]);
 
-        $response->assertStatus(201);
-        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
+        $response->assertJsonFragment([
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'amount' => 9750
+        ]);
 
+        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
         $this->assertTrue($concert->hasOrderFor('john@example.com'));
         $this->assertEquals(3, $concert->ordersFor('john@example.com')->first()->ticketQuantity());
     }
--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -9,6 +9,18 @@ class OrderTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function testConvertToAnArray()
+    {
+        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(5);
+        $order = $concert->orderTickets('jane@example.com', 5);
+        $result = $order->toArray();
+        $this->assertEquals([
+            'email' => 'jane@example.com',
+            'ticket_quantity' => 5,
+            'amount' => 6000
+        ], $result);
+    }
+
     public function testTicketsAreReleasedWhenAnOrderIsCancelled()
     {
         $concert = factory(Concert::class)->create()->addTickets(10);
