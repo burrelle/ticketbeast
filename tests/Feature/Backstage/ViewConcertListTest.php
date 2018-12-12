e2180c4c89c1f5961b910cf870bc1d46e3620f2f
Evan Burrell <burrelle@oregonstate.edu>
2018-12-12 11:28:30 +0000

    Listing a promoters concert

---
+++ tests/Feature/Backstage/ViewConcertListTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Backstage;
+
+use App\User;
+use App\Concert;
+use Tests\TestCase;
+use PHPUnit\Framework\Assert;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ViewConcertListTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected function setUp()
+    {
+        parent::setUp();
+         TestResponse::macro('data', function ($key) {
+            return $this->original->getData()[$key];
+        });
+         Collection::macro('assertContains', function ($value) {
+            Assert::assertTrue($this->contains($value), "Failed asserting that the collection contains the specified value.");
+        });
+         Collection::macro('assertNotContains', function ($value) {
+            Assert::assertFalse($this->contains($value), "Failed asserting that the collection does not contain the specified value.");
+        });
+    }
+
+    public function testGuestsCannotViewPromotersConcertList()
+    {
+        $response = $this->get('/backstage/concerts');
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function testPromotersCanOnlyViewListOfTheirOkwnConcerts()
+    {
+        $this->disableExceptionHandling();
+        $user = factory(User::class)->create();
+        $otherUser = factory(User::class)->create();
+        $concertA = factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertB = factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertC = factory(Concert::class)->create(['user_id' => $otherUser->id]);
+        $concertD = factory(Concert::class)->create(['user_id' => $user->id]);
+        $response = $this->actingAs($user)->get('/backstage/concerts');
+        $response->assertStatus(200);
+        $response->data('concerts')->assertContains($concertA);
+        $response->data('concerts')->assertContains($concertB);
+        $response->data('concerts')->assertContains($concertD);
+        $response->data('concerts')->assertNotContains($concertC);
+    }
+}
